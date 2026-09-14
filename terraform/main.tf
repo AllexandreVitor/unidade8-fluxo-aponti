de4da43f296dcc45677a9c478f756261f42932c9
AllexandreVitor <allexandrevitor.av@gmail.com>
2026-09-14 08:47:02 -0300

    chore(terraform): configurar provider AWS para LocalStack

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,12 @@ terraform {
 
   required_version = ">= 1.5.0"
 }
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+
+  endpoints {
+    s3 = "http://s3.localhost.localstack.cloud:4566"
+  }
