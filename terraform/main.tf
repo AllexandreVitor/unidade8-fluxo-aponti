41a559cd87c235ad9b0dfc582d906e648b4a2957
AllexandreVitor <allexandrevitor.av@gmail.com>
2026-09-14 09:59:17 -0300

    feat(terraform): habilitar criptografia KMS no bucket S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,3 +75,14 @@ resource "aws_kms_key" "documentos" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "documentos" {
+  bucket = aws_s3_bucket.documentos.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.documentos.arn
+    }
+  }
+}
