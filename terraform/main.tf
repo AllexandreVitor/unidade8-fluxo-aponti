2d30cc0a69962bbc67d6392a72640d70f20d52b0
AllexandreVitor <allexandrevitor.av@gmail.com>
2026-09-14 09:26:00 -0300

    feat(terraform): bloquear acesso público ao bucket S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,12 @@ provider "aws" {
 resource "aws_s3_bucket" "documentos" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_public_access_block" "documentos" {
+  bucket = aws_s3_bucket.documentos.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
